16d02ce8cc3b37bbcfcdb255dee6076c34c89894
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2016-12-07 18:05:40 -0500

    add undo

--- core.js
+++ core.js
@@ -1,22 +1,44 @@
-var ReduxUndo = window.ReduxUndo.default;
-
-function greeter(state, action) {
+function reducer(state, action) {
   if (typeof state === 'undefined') {
-    return 'World';
+    var name = 'World';
+    return {name: name, history: [name]};
   }
 
-  return action.name;
+  switch (action.type) {
+    case 'RESET':
+      var new_history = state.history.slice();
+      new_history.push(action.name);
+      return {name: action.name, history: new_history};
+    case 'UNDO':
+      var new_history = state.history.slice();
+      new_history.pop();
+      return {name: new_history[new_history.length - 1], history: new_history};
+    default:
+      return state;
+  }
 }
 
-var store = Redux.createStore(greeter);
+var store = Redux.createStore(reducer);
 
 function render() {
-  $('#output').html(store.getState().toString());
+  var state = store.getState();
+  var name = state.name;
+  $('#input').val(name);
+  $('#output').html(name);
+  $('#undo').prop('disabled', state.history.length == 1)
 }
 
 render();
 store.subscribe(render);
 
 $('#input').change(function () {
-  store.dispatch({ type: 'RESET', name: $(this).val()});
+  store.dispatch({
+    type: 'RESET',
+    name: $(this).val()
+  });
+});
+$('#undo').click(function () {
+  store.dispatch({
+    type: 'UNDO'
+  })
 });
