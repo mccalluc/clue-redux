cc942e16902c4863fbbb37e025ee99e3c101a31c
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2016-12-07 18:20:58 -0500

    Redo

--- core.js
+++ core.js
@@ -1,18 +1,36 @@
 function reducer(state, action) {
   if (typeof state === 'undefined') {
     var name = 'World';
-    return {name: name, history: [name]};
+    return {name: name, history: [name], future: []};
   }
 
   switch (action.type) {
     case 'RESET':
       var new_history = state.history.slice();
       new_history.push(action.name);
-      return {name: action.name, history: new_history};
+      return {
+        name: action.name,
+        history: new_history,
+        future: []
+      };
     case 'UNDO':
       var new_history = state.history.slice();
-      new_history.pop();
-      return {name: new_history[new_history.length - 1], history: new_history};
+      var new_future = state.future.slice();
+      new_future.unshift(new_history.pop());
+      return {
+        name: new_history[new_history.length - 1],
+        history: new_history,
+        future: new_future
+      };
+    case 'REDO':
+      var new_history = state.history.slice();
+      var new_future = state.future.slice();
+      new_history.push(new_future.shift());
+      return {
+        name: new_history[new_history.length - 1],
+        history: new_history,
+        future: new_future
+      };
     default:
       return state;
   }
@@ -25,7 +43,8 @@ function render() {
   var name = state.name;
   $('#input').val(name);
   $('#output').html(name);
-  $('#undo').prop('disabled', state.history.length == 1)
+  $('#undo').prop('disabled', state.history.length == 1);
+  $('#redo').prop('disabled', state.future.length == 0);
 }
 
 render();
@@ -40,5 +59,10 @@ $('#input').change(function () {
 $('#undo').click(function () {
   store.dispatch({
     type: 'UNDO'
+  })
+});
+$('#redo').click(function () {
+  store.dispatch({
+    type: 'REDO'
   })
 });
